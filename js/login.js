a4db3a45e457bc5e1d618af29b9848fb44b8c42f
basinagovardhan <govardhanb180@gmail.com>
2024-07-23 10:48:55 +0530

    creating new pages

--- js/login.js
+++ js/login.js
@@ -1,10 +1,35 @@
 function login_user(){
 
 
-    alert('login user Sussessfully')
+    debugger;
+const username=document.getElementById('username').value;
+const password=document.getElementById('password').value;
+
+
+    if (username==='nani' && password==='123') {
+
+        // console.log(username,password);
+        alert('login user Sussessfully')
+
+
+        window.location.assign("nani.html");
+
+
+    }
+
 }
 
 function cancel(){
 
-    alert("cancel")
+    alert("cancel");
+    window.location.assign("login.html");
+
+}
+
+
+
+
+function signup(){
+    debugger;
+    alert('Account Successfully Created')
 }
---
+++ js/signup.js
@@ -0,0 +1,10 @@
+
+const username=document.getElementById('signup').value;
+const password=document.getElementById('password').value;
+
+
+
+function signup(){
+    debugger;
+    alert('Account Successfully Created')
+}
